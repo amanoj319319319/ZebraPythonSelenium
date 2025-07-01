4d829dad587a67cd2a08bd478e803ffc10e04c9a
Ubuntu <ubuntu@ip-172-31-4-244.ap-southeast-2.compute.internal>
2025-07-01 18:07:23 +0000

    ubuntu first push

--- conftest.py
+++ conftest.py
@@ -4,10 +4,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service  # fixed import
 from Utilities.config_reader import get_base_url
+from selenium.webdriver.chrome.options import Options
 import logging
 
 logging.basicConfig(
-    filename="Logs/test_log.log",
+    filename="Logs\test_log.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
@@ -15,7 +16,13 @@ logging.basicConfig(
 
 @pytest.fixture(scope="function")
 def setup():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--disable-gpu")  # Recommended for Windows
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--no-sandbox")  # For Linux environments
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Avoids shared memory issues in Docker/Linux
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(8)
     driver.get(get_base_url())
